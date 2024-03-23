f156ccbb4e9598c7c6cb2655ba76a54aaf4b067b
codewithalihamza <512syedalihamza@gmail.com>
2024-03-23 23:22:21 +0500

    providing meta data to whole project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { metadata } from '.';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>{metadata.title?.toString()}</title>
+        <meta name="description" content={metadata.description?.toString()} />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
